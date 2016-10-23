8ab14afce9cb92cfc294f2c9178134f0503fc8bc
Rob Berwick <rob.berwick@gmail.com>
2016-10-23 13:17:48 +0100

    whitespace changes

--- vesc_datalogger.ino
+++ vesc_datalogger.ino
@@ -47,55 +47,55 @@ void setup() {
 	//--------RTC SETUP ------------
  	Rtc.Begin();
 
-    // if you are using ESP-01 then uncomment the line below to reset the pins to
-    // the available pins for SDA, SCL
-    // Wire.begin(0, 2); // due to limited pins, use pin 0 and 2 for SDA, SCL
+  // if you are using ESP-01 then uncomment the line below to reset the pins to
+  // the available pins for SDA, SCL
+  // Wire.begin(0, 2); // due to limited pins, use pin 0 and 2 for SDA, SCL
 
-    RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
-    Serial.println(formatDateTime(compiled));
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  Serial.println(formatDateTime(compiled));
 
-    if (!Rtc.IsDateTimeValid()) 
-    {
-        // Common Cuases:
-        //    1) first time you ran and the device wasn't running yet
-        //    2) the battery on the device is low or even missing
+  if (!Rtc.IsDateTimeValid()) 
+  {
+      // Common Cuases:
+      //    1) first time you ran and the device wasn't running yet
+      //    2) the battery on the device is low or even missing
 
-        Serial.println("RTC lost confidence in the DateTime!");
+      Serial.println("RTC lost confidence in the DateTime!");
 
-        // following line sets the RTC to the date & time this sketch was compiled
-        // it will also reset the valid flag internally unless the Rtc device is
-        // having an issue
+      // following line sets the RTC to the date & time this sketch was compiled
+      // it will also reset the valid flag internally unless the Rtc device is
+      // having an issue
 
-        Rtc.SetDateTime(compiled);
-    }
+      Rtc.SetDateTime(compiled);
+  }
 
-    if (!Rtc.GetIsRunning())
-    {
-    	Serial.println("RTC was not actively running, starting now");
-    	Rtc.SetIsRunning(true);
-    }
+  if (!Rtc.GetIsRunning())
+  {
+  	Serial.println("RTC was not actively running, starting now");
+  	Rtc.SetIsRunning(true);
+  }
 
-    RtcDateTime now = Rtc.GetDateTime();
-    if (now < compiled) 
-    {
-    	Serial.println("RTC is older than compile time!  (Updating DateTime)");
-    	Rtc.SetDateTime(compiled);
-    }
-    else if (now > compiled) 
-    {
-    	Serial.println("RTC is newer than compile time. (this is expected)");
-    }
-    else if (now == compiled) 
-    {
-    	Serial.println("RTC is the same as compile time! (not expected but all is fine)");
-    }
+  RtcDateTime now = Rtc.GetDateTime();
+  if (now < compiled) 
+  {
+  	Serial.println("RTC is older than compile time!  (Updating DateTime)");
+  	Rtc.SetDateTime(compiled);
+  }
+  else if (now > compiled) 
+  {
+  	Serial.println("RTC is newer than compile time. (this is expected)");
+  }
+  else if (now == compiled) 
+  {
+  	Serial.println("RTC is the same as compile time! (not expected but all is fine)");
+  }
 
-    // never assume the Rtc was last configured by you, so
-    // just clear them to your needed state
-    Rtc.Enable32kHzPin(false);
-    Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone); 
+  // never assume the Rtc was last configured by you, so
+  // just clear them to your needed state
+  Rtc.Enable32kHzPin(false);
+  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone); 
 
-    #ifdef DEBUG
+  #ifdef DEBUG
 	//SEtup debug port
 	Serial.begin(115200);
 	#endif
